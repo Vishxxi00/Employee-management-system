5f21bc79ae3b22ed90e54e133282d86b8b3997b5
Vishxxi00 <imankaj2000@gmail.com>
2022-04-03 11:07:45 +0530

    commit

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>All rights reserved</b>
+    </div>
+    <strong>Copyright &copy; 2021 <a href="">LANKA TOOLS (PVT) LTD</a></strong>
+</footer>
